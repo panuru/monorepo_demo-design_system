70fdd57034c1363001732f5fd89f32313e59a409
Maria Kuzovchikova <maria.kuzovchikova@flaconi.de>
2019-02-07 18:04:05 +0100

    Added common ui components with storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
+import { storiesOf, addDecorator } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { CssReset, GlobalCss, Themes } from '../style';
+import { Button, Container, Header } from '../components';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+addDecorator(withThemesProvider([Themes.default]))
+addDecorator(storyFn => (
+  <div>
+    <CssReset />
+    <GlobalCss />
+    { storyFn() }
+  </div>
+));
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('Container', module)
+  .add('default', () => <Container>Hello I'm a Container</Container>)
+
+storiesOf('Header', module)
+  .add('default', () => <Header/>);
